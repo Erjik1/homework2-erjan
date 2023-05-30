ce465ef64eb1a3a115ae2e4ba321cc75ca7d22e7
Erjik1 <120021425+Erjik1@users.noreply.github.com>
2023-05-30 13:49:03 +0600

    hw2-erjan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Svetovor = () => {
+  const [isRed, setIsRed] = useState(true)
+
+  const handleClick = () => {
+    setIsRed(!isRed)
+  }
+
+  const squareStyle = {
+    width: '200px',
+    height: '200px',
+    backgroundColor: isRed ? 'red' : 'blue',
+    cursor: 'pointer'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <div
+            style={squareStyle}
+            onClick={handleClick}
+        />
+        <div
+            style={{ ...squareStyle, backgroundColor: isRed ? 'blue' : 'red' }}
+            onClick={handleClick}
+        />
+      </div>
+  )
 }
 
-export default App;
+export default Svetovor;
